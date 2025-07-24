35fb68e291ef2e57553da2cffae67a884e447a5d
JMaxRowe <jmaxrowe@gmail.com>
2025-07-24 13:30:45 +0100

    styled playbar, added current song title to it

--- public/js/player.js
+++ public/js/player.js
@@ -1,10 +1,17 @@
     document.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById('global-audio')
+    const playPauseIcon = document.getElementById('playPauseIcon')
+    const titleEl = document.getElementById('currentSongTitle')
     if (!audio) return
 
     const savedSrc   = localStorage.getItem('currentTrack')
     const savedTime  = parseFloat(localStorage.getItem('currentTime') || 0)
     const wasPlaying = localStorage.getItem('isPlaying') === 'true'
+    const savedTitle = localStorage.getItem('currentTitle')
+
+    if(savedTitle){
+        titleEl.textContent=savedTitle
+    }
 
     if (savedSrc) {
         audio.src = savedSrc
@@ -29,31 +36,45 @@
         localStorage.setItem('isPlaying', 'false')
     })
 
-    document.querySelectorAll('.play-btn').forEach(btn => {
-        btn.addEventListener('click', event => {
-        const src = event.currentTarget.closest('.song-item').dataset.src
-        if (!src) return
-        audio.src = src
-        audio.play().catch(() => {})
-        })
-    })
-    const playPauseIcon = document.getElementById('playPauseIcon')
+    
+    
 
-    if (playPauseIcon){
-        function updateIcon(isPlaying) {
-            playPauseIcon.classList.toggle('fa-play', !isPlaying)
-            playPauseIcon.classList.toggle('fa-pause', isPlaying)
-        }
-    }
+function updateIcon(isPlaying) {
+    if(!playPauseIcon) return;
+    playPauseIcon.classList.toggle('fa-play', !isPlaying)
+    playPauseIcon.classList.toggle('fa-pause', isPlaying)
+ }
+
 
     updateIcon (wasPlaying)
+
+    audio.addEventListener('play',  () => updateIcon(true))
+    audio.addEventListener('pause', () => updateIcon(false))
 
     playPauseIcon.addEventListener('click', () =>{
         if (audio.paused) audio.play().catch(()=>{})
         else audio.pause()
     })
 
-    audio.addEventListener('play',  () => updateIcon(true))
-    audio.addEventListener('pause', () => updateIcon(false))
+    document.querySelectorAll('.play-btn').forEach(btn => {
+        btn.addEventListener('click', event => {
+            const item = event.currentTarget.closest('.song-item')
+            const src = item?.dataset.src
+            const title = item?.querySelector('h4')?.textContent
+
+        if (!src) return
+        audio.src = src
+
+        if (title){
+            titleEl.textContent = title
+            localStorage.setItem('currentTitle', title)
+        }
+        audio.play().catch(() => {})
+        })
+    })
+
+    
+
+    
 })
 
